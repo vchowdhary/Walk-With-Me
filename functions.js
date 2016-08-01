0e7fe856247f3164624c6af2312edd92e7fcefed
vchowdhary <vanshikachowdhary@gmail.com>
2016-08-01 12:45:30 -0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,15 +1,1 @@
 // JavaScript Document
-function changeTips(){
-	var i = 0;
-	while(document.getElementById("start-button").clicked === false){
-		var tips = ["hi", "get rekt"];
-		var text = document.getElementById("tips").innerHTML;
-		text = tips[i];
-		window.setTimeout(changeTips(), 1000);
-		i++;
-		if (i>tips.length){
-			i = 0;	
-		}
-	}
-}
-changeTips();
